1691b4b06442d66e2ada0a035e5e16ddca2f5e6c
Liron Sade <liron.sade@mail.huji.ac.il>
2018-10-03 21:08:12 +0300

    create array instead of print

--- parser.py
+++ parser.py
@@ -1,12 +1,11 @@
-sen = 'Find two numbers whose sum is 62 and whose difference is 6.'
+import re
 key_words_num = {'one':1, 'two':2, 'three':3}
 key_words_op = {'difference':'-', 'sum':'+'}
 
 
-def parse_sen():
+def parse_sen(sen):
     arr_sen = sen.split()
     my_equi = []
-    # my_params =
     num_params = 0
     curr_op = ''
     for i, word in enumerate(arr_sen):
@@ -15,10 +14,12 @@ def parse_sen():
         if word in key_words_op.keys():
             curr_op = key_words_op[word]
         if word == 'is':
-            create_smpl(num_params, curr_op, arr_sen[i+1])
+            my_equi.append(create_eq(num_params, curr_op, arr_sen[i+1]))
+    return my_equi
 
 
-def create_smpl(num_params, op, num):
+def create_eq(num_params, op, num):
+    num = re.findall('\d+', num)[0]
     optional_params = ['x', 'y', 'z']
     equi = ''
     for i in range(num_params):
@@ -27,6 +28,8 @@ def create_smpl(num_params, op, num):
             equi += op
 
     equi += '=' + num
-    print(equi)
+    return equi
 
-parse_sen()
+if __name__ == '__main__':
+    sen = 'Find two numbers whose sum is 62 and whose difference is 6.'
+    print(parse_sen(sen))
