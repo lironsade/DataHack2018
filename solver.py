dd1dcda61c92fb5adf70ab8dfe8e1297e80e9ad0
Arikshtine <arik.shtine@gmail.com>
2018-10-04 11:32:26 +0300

    update

--- solver.py
+++ solver.py
@@ -1,12 +1,12 @@
 # input: ['x+y - 40', '2*x+4*y - 108'] -> list of strings, ['x', 'y'] -> list of strings
-# output: '{(26,14)}' -> set of intgers
+# output: '{(26,14)}' -> FiniteSet of Integers
 
 import sympy as sy
 
 
 def solver(equations_lst, unkn_lst):
     A, b = build_matrix(equations_lst, unkn_lst)
-    solution = set(sol(A, b, unkn_lst))
+    solution = sol(A, b, unkn_lst)
     return solution
 
 
@@ -21,4 +21,4 @@ def sol(A, b, unkn_lst):
 if __name__ == '__main__':
     equations = ['x+y - 40', '2*x+4*y - 108']
     unkn = ['x', 'y']
-    print(set(solver(equations, unkn)))
+    print(solver(equations, unkn))
