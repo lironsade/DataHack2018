03387b0ea0afd3049c86a013d23f34636d4893dd
Liron Sade <liron.sade@mail.huji.ac.il>
2018-10-04 13:03:51 +0300

    refactor

--- parser.py
+++ parser.py
@@ -6,29 +6,36 @@ key_words_create = {'of', 'is', 'by'}
 
 
 def parse_sen(sen):
-    arr_sen = sen.replace('  ', ' ').split()
-    arr_sen = [x.lower() for x in arr_sen]
-    my_equi = []
+    """
+    parses a mathematical question sentence into equations.
+    """
+    sen = sen.lower() # lowercase
+    arr_sen = sen.replace('  ', ' ').split() # breakdown to words
+    # arr_sen = [x.lower() for x in arr_sen]
+    my_equis = []
     num_params = 0
     curr_op = ''
     for i, word in enumerate(arr_sen):
         if word in key_words_params:
             if arr_sen[i-1] in key_words_num.keys():
                 num_params = key_words_num[arr_sen[i-1]]
-            if arr_sen[i-1].isdigit():
+            elif arr_sen[i-1].isdigit():
                 num_params = arr_sen[i-1]
-        if word in key_words_op.keys():
+        elif word in key_words_op.keys():
             curr_op = key_words_op[word]
-        if (word in key_words_create) and is_num(arr_sen[i+1]):
+        elif (word in key_words_create) and is_num(arr_sen[i+1]):
             eq = create_eq(num_params, curr_op, rmv_non_digits(arr_sen[i+1]))
             if eq:
-                my_equi.append(eq)
-        if word == 'another':
+                my_equis.append(eq)
+        elif word == 'another':
             num_params += 1
-    return my_equi
+    return my_equis
 
 
 def create_eq(num_params, op, num):
+    '''
+    creates eq in the form of: param1 [op] param2 [op]... = num
+    '''
     optional_params = ['x', 'y', 'z']
     equi = ''
 
