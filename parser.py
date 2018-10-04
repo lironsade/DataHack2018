222947b20ce0cc839533b1856ce3e5d51f9175d2
danity251 <danity251@gmail.com>
2018-10-04 17:53:25 +0300

    another type of sentences

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 import spacy
 import solver
-key_words_num = {'one':1, 'two':2, 'three':3, 'One':1, 'Two':2, 'Three':3}
-key_words_op = {'difference':'-', 'sum':'+', 'exceeds':'-', 'less than':'-'}
+key_words_num = {'one':1, 'two':2, 'three':3, 'One':1, 'Two':2, 'Three':3, 'A':1, 'a':1}
+key_words_op = {'difference':'-', 'sum':'+', 'exceeds':'-', 'less than':'-', 'less':'-'}
 key_words_params = {'numbers', 'number', 'integers', 'integer'}
 key_words_create = {'of', 'is', 'by'}
 op_params = ['x', 'y', 'z', 'w']
@@ -20,22 +20,28 @@ def parse_sen(sen):
     parses a mathematical question sentence into equations.
     """
     nlp = spacy.load('en_core_web_sm')
+    is_another = 'another' in sen
+    params_found = False
     nlp_sen = nlp(sen.replace('  ', ' '))
     my_equis = []
-    num_params = 0
     params = []
     curr_op = ''
     for i, word in enumerate(nlp_sen):
-        if word.text in key_words_params:
+        if word.text in key_words_params and not params_found:
             params = create_params(word.lefts)
+            params_found = True
         if word.text in key_words_op.keys():
             curr_op = key_words_op[word.text]
-        if (word.text in key_words_create) and is_num(nlp_sen[i+1].text):
+        if word.text in key_words_create and is_num(nlp_sen[i+1].text):
+            if is_another:
+                params.append('y')
+                is_another = False
+            if curr_op == '':
+                curr_op = key_words_op[str(list(word.rights)[0])]
             eq = create_eq(params, curr_op, rmv_non_digits(nlp_sen[i+1].text))
             if eq:
                 my_equis.append(eq)
-        if word.text == 'another':
-            params.append('y')
+                curr_op = ''
     return my_equis
 
 
@@ -67,7 +73,7 @@ def create_eq(params, op, num):
     '''
     if not params:
         return
-    
+
     equi = ''
     length = len(params)
 
@@ -89,7 +95,8 @@ def rmv_non_digits(num):
 
 
 if __name__ == '__main__':
-    sen = 'The sum of 2 integers is 36. The difference of the same 2 numbers is 4. Find the integers.'
+    sen = 'A number is 12 less than another.  The sum of the numbers is 28.  find the Numbers.'
     # sen = 'The sum of three consecutive odd integers is -273. What are the integers?'
     print(parse_sen(sen))
     # print(create_eq(['x', 'y'], '+', '6'))
+    # print(key_words_op['less'])
