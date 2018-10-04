af3a9c1807b99e665e9393bedce1a184388afc9c
Liron Sade <liron.sade@mail.huji.ac.il>
2018-10-04 13:22:46 +0300

    added a starting point

---
+++ model.py
@@ -0,0 +1,18 @@
+# Useage:
+# solve_problem(sentence)
+
+from parser import parse_sen
+from solver import solve
+
+def solve_problem(sen):
+    '''
+    returns a set of numbers that should be the result of the sentence.
+    '''
+    eqs = parse_sen(sen)
+    print(eqs)
+    return solve(eqs, ['x', 'y'])
+
+
+if __name__ == '__main__':
+    sen = 'the sum of two numbers is 68. their difference is 16. what are the numbers?'
+    print(solve_problem(sen))
