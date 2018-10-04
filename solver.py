b720c785c2673447aa2105220f2de5c0ab97825d
Arikshtine <arik.shtine@gmail.com>
2018-10-04 15:05:21 +0300

    update of some functions for danit

--- solver.py
+++ solver.py
@@ -13,7 +13,25 @@ def replace_equ(equations_lst):
     return [(s.replace("=", "-(") + ')') for s in equations_lst]
 
 
+def double_num(unkn_lst):
+    return ["2*" + n for n in unkn_lst]
+
+
+def odd_num(unkn_lst):
+    return ["2*" + n + ' + 1' for n in unkn_lst]
+
+
+def consecutive_num(unkn_lst):
+    lst = []
+    counter = 1
+    for n in unkn_lst:
+        lst.append(n + ' + ' + str(counter))
+        counter += 1
+    return lst
+
+
 if __name__ == '__main__':
     equations_smp = ['x+y=68', 'x-y=16']
-    unkn = ['x', 'y']
-    print(solve(equations_smp, unkn))
+    unkn = ['x', 'x']
+    # print(solve(equations_smp, unkn))
+    print(consecutive_num(unkn))
