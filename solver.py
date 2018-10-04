c54401ecea02ff40f6aea9d87c8b35da65ddec0f
Arikshtine <arik.shtine@gmail.com>
2018-10-04 22:17:07 +0300

    solver updateeeee

--- solver.py
+++ solver.py
@@ -5,10 +5,29 @@ def solve(equations_lst, unkn_lst):
     """solves the problem from list of equation and list of Unknown values
     :param equations_lst: ['x+y = 40', '2*x+4*y = 108'] -> list of strings
     :param unkn_lst:  ['x', 'y'] -> list of strings
-    :return: {(26,14)}' -> FiniteSet of Integers
+    :return: {(26,14)}' -> list of float or ints
     """
+    unkn_lst_s = unkn_lst[:]
+    s_case = False
+    if handle_unkn(unkn_lst_s):
+        s_case = True
+        unkn_lst = ['x']
     A, b = sy.linear_eq_to_matrix(replace_equ(equations_lst), unkn_lst)
-    return sy.linsolve((A, b), unkn_lst)
+    sol = [eval(str(x)) for b in sy.linsolve((A, b), unkn_lst) for x in b]
+    if not s_case:
+        return sol
+    return [eval(i.replace('x', str(sol[0]))) for i in unkn_lst_s]
+
+
+def handle_unkn(unkn_lst):
+    """
+    :param unkn_lst: list of Unknown values
+    :return: if this shit special
+    """
+    for i in unkn_lst:
+        if len(i) > 1:
+            return True
+    return False
 
 
 def replace_equ(equations_lst):
@@ -50,8 +69,6 @@ def consecutive_num(unkn_lst):
 
 
 if __name__ == '__main__':
-    equations_smp = ['(2*k-2)+(2*k+2)=3*(2*k)-12']
-    # print(replace_equ(equations_smp))
-    unkn = ['2*k-2', '2*k', '2*k+2']
-    print(solve(equations_smp, unkn))
-    # print(solve(equations_smp, unkn))
+    equations_smp = ['2*(x) + 1+2*(x + 1) + 1+2*(x + 2) + 1=-273']
+    s_unkn = ['2*(x) + 1', '2*(x + 1) + 1', '2*(x + 2) + 1']
+    print(solve(equations_smp, s_unkn))
